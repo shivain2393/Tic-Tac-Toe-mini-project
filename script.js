7fe1844fa10d675183f9d2410517d2084ca7f4dc
Shivain Sagar <shivain.sagar@gmail.com>
2024-04-06 17:19:17 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ let turnO = true;
 
 
 let winPatterns = [
-    [0, 1, 3],
+    [0, 1, 2],
     [0, 3, 6],
     [0, 4, 8],
     [1, 4, 7],
-    [2, 5, 8],
     [2, 4, 6],
+    [2, 5, 8],
     [3, 4, 5],
     [6, 7, 8],
 ];
@@ -41,10 +41,10 @@ const resetGame = () => {
 const printResult = (isWinner, winner = null) => {
     turnText.style.display = "none"
     if(isWinner){
+        msg.style.display = "block";
         let Winner = document.querySelector("#winner");
         Winner.innerText =  winner;
         Winner.style.color = "#CDACA1"
-        msg.style.display = "block";
     }
     else{
         msg.innerText = `The game is a Draw!`;
